5cab5e899c511e4761f87e6c91e2552480e08f05
Marcos Trinca <marcostrinca@gmail.com>
2018-02-28 15:53:52 -0300

    DNN "n" layers to classify images

--- numpy-nn-concepts/3.deep_nn_n_layers.py
+++ numpy-nn-concepts/3.deep_nn_n_layers.py
@@ -1,22 +1,120 @@
+'''
+Toy DNN to classify images from Simpsons dataset: img has Homer(0) or Margie(1)?
+Dataset comes from Kaggle datasets (I just got Homer and Margie images)
+I selected only images with format 480 X 320
+'''
+
+import time, sys, os
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
+import scipy
+from sklearn.model_selection import train_test_split
+from PIL import Image
+from scipy import ndimage
 from _utils_dnn import *
 
-def load_data():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+def load_data(path):
 
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+	X = []
+	Y = []
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+	idx_classes = 0
+	idx_files = 0
+	for folder_name in os.listdir(path):
+
+		for filename in os.listdir(path+"/"+folder_name):
+
+			img = Image.open(path+"/"+folder_name+"/"+filename)
+			percent = 0.25
+			img = img.resize( [int(percent * s) for s in img.size] )
+
+			img = np.asarray(img)
+
+			if img.shape[0] == 120 and img.shape[1] == 80:
+				X.append(img)
+				Y.append(idx_classes)
+
+		idx_classes += 1
+
+	X = np.array(X)
+	Y = np.array(Y)
+
+	# reshape to get (m, n_x) and (m, n_y)
+	X = X.reshape(-1, 120 * 80 * 3) / 255
+	Y = Y.reshape(-1, 1)
+
+	return X, Y
+
+X, Y = load_data("./data/dataset_simpsons")
+# test_img = X[200]
+# plt.imshow(test_img)
+# plt.show()
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+
+# transpose to get (n_x, m) and (n_y, m)
+X_train = X_train.T
+Y_train = Y_train.T
+X_test = X_test.T
+Y_test = Y_test.T
+
+print(X_train.shape)
+print(Y_train.shape)
+print(X_test.shape)
+print(Y_test.shape)
+
+### DNN architecture
+layers_dims = [X_train.shape[0], 20, 7, 5, 1] #  5-layer model
+
+def model(X, Y, layers_dims, learning_rate=0.001, num_iterations=3000, print_cost=False):
     
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    costs = []
     
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    # Parameters initialization
+    parameters = initialize_parameters_deep(layers_dims)
+    
+    # Loop (gradient descent)
+    for i in range(0, num_iterations):
+
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID
+        AL, caches = L_model_forward(X, parameters)
+        
+        # Compute cost
+        cost = compute_cost(AL, Y)
+    
+        # Backward propagation
+        grads = L_model_backward(AL, Y, caches)
+ 
+        # Update parameters
+        parameters = update_parameters(parameters, grads, learning_rate)
+                
+        # Print the cost every 100 training example
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" % (i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+            
+    # plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learning_rate))
+    plt.show()
+    
+    return parameters
+
+parameters = model(X_train, Y_train, layers_dims, num_iterations = 1000, print_cost = True)
+
+pred_train = predict(X_train, Y_train, parameters)
+
+pred_test = predict(X_test, Y_test, parameters)
+
+print_mislabeled_images(classes, test_x, test_y, pred_test)
+
+
+
+
+
 
 
